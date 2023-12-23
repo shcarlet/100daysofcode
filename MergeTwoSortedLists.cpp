dcb6c3d68b80f5811dfebbaa237d1b9d793fa5d2
shcarlet <135805635+shcarlet@users.noreply.github.com>
2023-12-23 18:39:02 +0530

    Create MergeTwoSortedLists.cpp

---
+++ MergeTwoSortedLists.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* dummy= new ListNode(0);
+        ListNode* current =dummy;
+        while(l1!=NULL && l2!=NULL)
+        {
+            if(l1->val<=l2->val)
+            {
+                current->next=l1;
+                l1=l1->next;
+            }
+            else
+            {
+                current->next=l2;
+                l2=l2->next;
+            }
+            current=current->next;
+            }
+            current->next=l1!=NULL? l1:l2;
+            return dummy->next;
+    }
+};
