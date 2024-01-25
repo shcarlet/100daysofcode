9f8b2a6c7a8f19176e34d2db7ec9e10690c381b5
shcarlet <135805635+shcarlet@users.noreply.github.com>
2024-01-26 00:21:34 +0530

    Remove Linked List Elements

--- Day-029.cpp
+++ Day-029.cpp
@@ -8,3 +8,26 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode*list=new ListNode();
+        list->next=head;
+        ListNode*prev=list;
+        ListNode*curr=head;
+        while(curr!=NULL)
+        {
+            if(curr->val!=val)
+            {
+                prev=curr;
+                curr=curr->next;
+            }
+            else
+            {
+                prev->next=curr->next;
+                curr=prev->next;
+            }
+        }
+        return list->next;
+    }
+};
