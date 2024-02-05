d1215a5b1547e5a203f790e5ffe6bca7610b1f03
shcarlet <135805635+shcarlet@users.noreply.github.com>
2024-02-05 22:23:13 +0530

    finding duplicates

--- Day-033.cpp
+++ Day-033.cpp
@@ -1,1 +1,20 @@
-D
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            mp[nums[i]]++;
+        }
+        for(auto i:mp)
+        {
+            if(i.second>=2)
+            {
+                return true;
+            }
+        }  
+        return false;      
+    }
+};
+
