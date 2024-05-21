5aa76cab1b7a2707baa4170ab189f856f607c5a5
shcarlet <135805635+shcarlet@users.noreply.github.com>
2024-05-21 11:00:08 +0530

    Update Day-033.cpp finding duplicate

--- Day-033.cpp
+++ Day-033.cpp
@@ -4,7 +4,8 @@ public:
        unordered_set<int>s;
         for(int i=0;i<nums.size();i++)
             {
-                if(s.find(nums[i])!=nums[i])
+                if(s.find(nums[i])!=nums[i])   //S.find(val) iterates through s till it matches Val. if it doesn't it points at s.end
+                                               //S.find(val) != S.end() is true when val is present in s else it's false
                 {
                     return true;
                 }
