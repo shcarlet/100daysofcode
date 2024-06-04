f2fd1619f3aabeb646addfa1e424e793db95a3d7
shcarlet <135805635+shcarlet@users.noreply.github.com>
2024-06-04 23:28:25 +0530

    longest repeating substring with k number of replacements

---
+++ day-052.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        vector<int>count(26);
+        int i=0;
+        int j=0;
+        int maxcount=0;
+        int result=0;
+        while(j<s.size())
+        {
+            count[s[j]-'A']++;
+            maxcount=max(maxcount,count[s[j]-'A']);
+            if(j-i+1-maxcount>k)
+            {
+                count[s[i]-'A']--;
+                i++;
+            }
+            result=max(result,j-i+1);
+            j++;
+        }
+        return result;
+    }
+};
