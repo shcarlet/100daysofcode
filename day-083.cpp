139b42d09d1022d983b15393dc867dca6ede11b5
Srujana C Bhandary <135805635+shcarlet@users.noreply.github.com>
2024-07-03 12:54:50 +0530

    tasks scheduling
    time complexity of   assigning char in heap is O(26)
    overall

---
+++ day-083.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        unordered_map<char,int>mapin;       //alphabets and its freq
+        for(char t:tasks)
+        {
+            mapin[t]++;
+        }
+        priority_queue<int>maxheap;
+        for(auto& i:mapin)
+        {
+            maxheap.push(i.second);   //freaquency of alphabets
+        }
+        int t=0;
+        queue<pair<int,int>>q;
+        while(!maxheap.empty() || !q.empty())
+        {
+            if(!q.empty() && t>=q.front().second)
+            {
+                maxheap.push(q.front().first);   //freq
+                q.pop();
+            }
+            if(!maxheap.empty())
+            {
+                int freq=maxheap.top()-1;
+                maxheap.pop();
+                if(freq>0)
+                {
+                    q.push({freq,t+n+1});
+                }
+            }
+            t++;
+        }
+        return t;
+    }
+};
