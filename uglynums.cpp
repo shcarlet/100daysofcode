21e7a3bdc6ebdf4fcdf4a7a2c77de280573e9bf8
Srujana C Bhandary <135805635+shcarlet@users.noreply.github.com>
2024-07-19 22:04:46 +0530

    Create uglynums.cpp

---
+++ uglynums.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+      if(n<=0)
+      {
+        return false;
+      }
+      int primes[]={2,3,5};
+      for(int p : primes)
+      {
+        while(n%p==0)
+        {
+            n=n/p;
+        }
+      }
+      return n==1;
+       
+    }
+};
